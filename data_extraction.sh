9bde973e6359990f739e17fc704de29d9de8ed6e
Max <titantbx1989215@gmail.com>
2014-09-10 00:02:52 +1200

    data extraction

--- data_extraction.sh
+++ data_extraction.sh
@@ -1,26 +1,71 @@
 #!/bin/bash
 
-cat truancy.csv | cut -d"," -f2- > truancy_2.csv
-sed -i 's/"//g' truancy_2.csv
-sed -i 's/ //g' truancy_2.csv
-mv truancy_2.csv truancy.csv
-sed -i 's/ST03Q02/BirthYear/g' truancy.csv
-sed -i 's/ST04Q01/Gender/g' truancy.csv
-sed -i 's/ST07Q01/Repeat<01>/g' truancy.csv
-sed -i 's/ST07Q02/Repeat<02>/g' truancy.csv
-sed -i 's/ST07Q03/Repeat<03>/g' truancy.csv
+format() {
+	cat $1 | cut -d"," -f2- > $2
+	sed -i 's/"//g' $2
+	sed -i 's/ //g' $2
+	mv $2 $1
+}
+rename_common() {
+	sed -i 's/ST03Q02/BirthYear/g' $1
+	sed -i 's/ST04Q01/Gender/g' $1
+}
+
+question_28() {
+	sed -i 's/ST29Q01/EnjoyReading/g' $1
+	sed -i 's/ST29Q02/WorthwhileWork/g' $1
+	sed -i 's/ST29Q03/LookForwardLesson/g' $1
+	sed -i 's/ST29Q04/EnoyMath/g' $1
+	sed -i 's/ST29Q05/CareerChances/g' $1
+	sed -i 's/ST29Q06/Interested/g' $1
+	sed -i 's/ST29Q07/FutureStudy/g' $1
+	sed -i 's/ST29Q08/GetJob/g' $1
+}
+
+parents_opinion() {
+	sed -i 's/ST35Q04/ParentsBelieveImportant/g' $1
+	sed -i 's/ST35Q05/ParentsBelieveCareer/g' $1
+	sed -i 's/ST35Q06/ParentsLikeMath/g' $1
+}
+
+repeat_grade() {
+	sed -i 's/ST07Q01/Repeat<01>/g' $1
+	sed -i 's/ST07Q02/Repeat<02>/g' $1
+	sed -i 's/ST07Q03/Repeat<03>/g' $1
+}
+
+parents_education() {
+	sed -i 's/ST13Q01/MotherEducation/g' $1
+	sed -i 's/ST17Q01/FatherEducation/g' $1
+}
+format truancy.csv truancy_2.csv
+rename_common truancy.csv
+repeat_grade truancy.csv
 sed -i 's/ST08Q01/LateForSchool/g' truancy.csv
 sed -i 's/ST09Q01/SkipWholeDay/g' truancy.csv
 sed -i 's/ST115Q01/SkipClasses/g' truancy.csv
 
-cat late.csv | cut -d"," -f2- > late_2.csv
-sed -i 's/"//g' late_2.csv
-sed -i 's/ //g' late_2.csv
-mv  late_2.csv late.csv
+format parents_luxuries.csv parents_luxuries_2.csv
+rename_common parents_luxuries.csv
+parents_education parents_luxuries.csv
+sed -i 's/books//g' parents_luxuries.csv
+sed -i 's/ST26Q13/Dishwasher/g' parents_luxuries.csv
+sed -i 's/ST27Q02/Tv/g' parents_luxuries.csv
+sed -i 's/ST27Q03/Computers/g' parents_luxuries.csv
+sed -i 's/ST27Q04/Cars/g' parents_luxuries.csv
+sed -i 's/ST27Q05/Showers/g' parents_luxuries.csv
+sed -i 's/ST28Q01/Books/g' parents_luxuries.csv
 
-cat parents_luxuries.csv | cut -d"," -f2- > parents_luxuries_2.csv
-sed -i 's/"//g' parents_luxuries_2.csv
-sed -i 's/ //g' parents_luxuries_2.csv
-sed -i 's/books//g' parents_luxuries_2.csv
-mv  parents_luxuries_2.csv parents_luxuries.csv
+format job_interest.csv job_interest_2.csv
+rename_common job_interest.csv
+question_28 job_interest.csv
 
+format parents_stu_math.csv parents_stu_math_2.csv
+rename_common parents_stu_math.csv
+question_28 parents_stu_math.csv
+parents_opinion parents_stu_math.csv
+
+format parents_edu_att_math.csv parents_edu_att_math_2.csv
+rename_common parents_edu_att_math.csv
+parents_opinion parents_edu_att_math.csv
+parents_education parents_edu_att_math.csv
--- extraction.r
+++ extraction.r
@@ -3,14 +3,19 @@ library(data.table)
 load("pisaData.rda")
 attach(student2012)
 
-truancy <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST09Q01", "ST115Q01")]
+truancy <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST08Q01", "ST09Q01", "ST115Q01")]
 
-late <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST08Q01")]
-
+job_interest <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST29Q01", "ST29Q02", "ST29Q03", "ST29Q04", "ST29Q05", "ST29Q06", "ST29Q07", "ST29Q08")]
 
 parents_luxuries <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST13Q01", "ST17Q01", "ST26Q13", "ST27Q02", "ST27Q03", "ST27Q04", "ST27Q05", "ST28Q01")]
 
+parents_stu_math <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST29Q01", "ST29Q02", "ST29Q03", "ST29Q04", "ST29Q05", "ST29Q06", "ST29Q07", "ST29Q08", "ST35Q04", "ST35Q05", "ST35Q06")]
 
-write.csv(late, "late.csv")
+parents_edu_att_math <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST35Q04", "ST35Q05", "ST35Q06", "ST13Q01", "ST17Q01")]
+
+write.csv(parents_stu_math, "parents_stu_math.csv")
+write.csv(job_interest, "job_interest.csv")
 write.csv(truancy, "truancy.csv")
 write.csv(parents_luxuries, "parents_luxuries.csv")
+write.csv(parents_edu_att_math, "parents_edu_att_math.csv")
+
