3dcd4cb10b595368e177e6ac7dde047e5c3a9250
Max <titantbx1989215@gmail.com>
2014-09-10 18:41:05 +1200

    update truancy analysis

--- data_extraction.sh
+++ data_extraction.sh
@@ -29,21 +29,37 @@ parents_opinion() {
 }
 
 repeat_grade() {
-	sed -i 's/ST07Q01/Repeat<01>/g' $1
-	sed -i 's/ST07Q02/Repeat<02>/g' $1
-	sed -i 's/ST07Q03/Repeat<03>/g' $1
+	sed -i 's/ST07Q01/Repeat01/g' $1
+	sed -i 's/ST07Q02/Repeat02/g' $1
+	sed -i 's/ST07Q03/Repeat03/g' $1
 }
 
 parents_education() {
 	sed -i 's/ST13Q01/MotherEducation/g' $1
 	sed -i 's/ST17Q01/FatherEducation/g' $1
 }
+
+sed -i 's/Yes, //g' truancy.csv
+sed -i 's/No, //g' truancy.csv
 format truancy.csv truancy_2.csv
 rename_common truancy.csv
 repeat_grade truancy.csv
 sed -i 's/ST08Q01/LateForSchool/g' truancy.csv
 sed -i 's/ST09Q01/SkipWholeDay/g' truancy.csv
 sed -i 's/ST115Q01/SkipClasses/g' truancy.csv
+cat truancy.csv | cut -d"," -f10 > skipclasses.csv
+sed -i 's/1/None/g' skipclasses.csv
+sed -i 's/2/Oneortwotimes/g' skipclasses.csv
+sed -i 's/3/Threeorfourtimes/g' skipclasses.csv
+sed -i 's/4/Fiveormoretimes/g' skipclasses.csv
+sed -i 's/^/,/g' skipclasses.csv
+cat truancy.csv | cut -d"," -f1,2,3,4,5,6,7,8,9 > truancy_2.csv
+rm truancy.csv
+paste truancy_2.csv skipclasses.csv > truancy.csv
+rm truancy_2.csv
+rm skipclasses.csv
+sed -i 's/\t//g' truancy.csv
+
 
 format parents_luxuries.csv parents_luxuries_2.csv
 rename_common parents_luxuries.csv
@@ -69,3 +85,12 @@ format parents_edu_att_math.csv parents_edu_att_math_2.csv
 rename_common parents_edu_att_math.csv
 parents_opinion parents_edu_att_math.csv
 parents_education parents_edu_att_math.csv
+
+format confidence_external_factors.csv confidence_external_factors_2.csv
+rename_common confidence_external_factors.csv
+sed -i 's/ST43Q01/EnoughEffort/g' confidence_external_factors.csv
+sed -i 's/ST43Q02/UptoMe/g' confidence_external_factors.csv
+sed -i 's/ST43Q05/CouldPerformWell/g' confidence_external_factors.csv
+sed -i 's/ST43Q03/FamilyProblems/g' confidence_external_factors.csv
+sed -i 's/ST43Q04/TeacherProblems/g' confidence_external_factors.csv
+sed -i 's/ST44Q05/MaterialTooHard/g' confidence_external_factors.csv
--- extraction.r
+++ extraction.r
@@ -1,11 +1,13 @@
-library(dplyr)
-library(data.table)
 load("pisaData.rda")
 attach(student2012)
 
-truancy <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST08Q01", "ST09Q01", "ST115Q01")]
+#truancy <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST08Q01", "ST09Q01", "ST115Q01")]
+
+truancy <- student2012[which(CNT=="Israel"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST07Q01", "ST07Q02", "ST07Q03", "ST08Q01", "ST09Q01", "ST115Q01")]
 
 job_interest <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST29Q01", "ST29Q02", "ST29Q03", "ST29Q04", "ST29Q05", "ST29Q06", "ST29Q07", "ST29Q08")]
+
+confidence_external_factors <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST43Q01", "ST43Q02", "ST43Q05", "ST43Q03", "ST43Q04", "ST44Q05")]
 
 parents_luxuries <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST13Q01", "ST17Q01", "ST26Q13", "ST27Q02", "ST27Q03", "ST27Q04", "ST27Q05", "ST28Q01")]
 
@@ -13,6 +15,7 @@ parents_stu_math <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDST
 
 parents_edu_att_math <- student2012[which(CNT=="China-Shanghai"), ][c("CNT", "STIDSTD", "ST03Q02", "ST04Q01", "ST35Q04", "ST35Q05", "ST35Q06", "ST13Q01", "ST17Q01")]
 
+write.csv(confidence_external_factors, "confidence_external_factors.csv")
 write.csv(parents_stu_math, "parents_stu_math.csv")
 write.csv(job_interest, "job_interest.csv")
 write.csv(truancy, "truancy.csv")
