8b5c1ee56cbaaa5d647f5ebf8542ec15dbd26a8a
Max <titantbx1989215@gmail.com>
2014-09-22 17:36:43 +1200

    up

--- Visual_1/visualize.js
+++ Visual_1/visualize.js
@@ -1,28 +1,35 @@
 function visualize(dataset) {
-		var scale = d3.scale.linear()
+		var xScale = d3.scale.linear()
 					.domain([0, d3.max(dataset)])
-					.range([0, height])
+					.range([0, width])
+
+
+		var xAxis = d3.svg.axis()
+					.scale(xScale)
+					.orient("bottom")
 
 		var svg = d3.select("body").append("svg")
 					.attr("width", width)
 					.attr("height", height)
+
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(0, " + (height - 20) + ")")
+			.call(xAxis)
+
 		svg.selectAll("rect")
 			.data(dataset)
 			.enter()
 			.append("rect")
 			.attr("x", 0)
 			.attr("y", function(d, i) {
-					return i * 30;
+					return i * 150;
 					})
 			.attr("width", function(d) {
-					return scale(d);
+					return xScale(d);
 					})
-			.attr("height", 10)
+			.attr("height", 100)
 			.attr("fill", "#3366ff")
 	}
 
-	d3.csv("truancy_summary.csv", function(data) {
-		var dataset = data_extract(data[0])
-		visualize(dataset)
-	})
 
